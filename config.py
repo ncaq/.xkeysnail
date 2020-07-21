453652549952dcc9a90a101061094a32da5a2aa5
ncaq <ncaq@ncaq.net>
2020-07-22 01:01:43 +0900

    added: LilyTerm: disable C-,

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ define_keymap(re.compile("Slack|discord"), {
     K("M-v"): [K("M-Up")],
 }, "Slack and Discord switch channel")
 
-define_keymap(re.compile("Slack|discord"), {
+define_keymap(re.compile("LilyTerm|Slack|discord"), {
     K("C-w"): K("MUHENKAN"),
 }, "C-,のショートカットを無効化しつつIMEオフを維持する")
 
